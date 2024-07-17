41de639607960cc850907ed05edaf45d1e08306b
bautistagorchs <bautista.gorchs@gmail.com>
2024-07-17 17:05:56 -0300

    configuracion inicial, instalar SASS, estructura de carpetas, e importar fuentes

---
+++ src/fuentes/fuentes.ts
@@ -0,0 +1,11 @@
+import { Source_Code_Pro, Open_Sans } from "next/font/google";
+
+export const source_code_pro = Source_Code_Pro({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const open_sans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
