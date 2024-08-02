41de05c5bdaead7871af9e32dc06ed2535ee6c32
bautistagorchs <bautista.gorchs@gmail.com>
2024-08-01 23:03:19 -0300

    agregar secciones proyecto y nuestra mision

--- src/componentes/landing/Landing.tsx
+++ src/componentes/landing/Landing.tsx
@@ -4,6 +4,9 @@ import React from "react";
 import s from "./landing.module.scss";
 import Image from "next/image";
 import img from "@/archivos/niño_parado_espaldas.jpg";
+import ccr from "@/archivos/foto_ccr_majo_jovenes.jpg";
+import bolson from "@/archivos/foto_bolson_pet.png";
+import barrioLimpio from "@/archivos/foto_casa_euge_pet.jpg";
 
 export default function Landing() {
   return (
@@ -55,7 +58,46 @@ export default function Landing() {
       </section>
       <section className={s.proyectos}>
         <div>
-          <div className={s.contenidoProyectos}></div>
+          <div className={s.contenidoProyectos}>
+            <h1>El corazón de nuestro trabajo</h1>
+            <div className={s.card}>
+              <Image src={ccr} alt="ccr" />
+              <h3>Centro Comunitario Resiliente</h3>
+              <p>
+                Este proyecto busca convertir Centros Comunitarios en
+                resilientes, logrando un impacto social y ambiental positivo. Se
+                enfocará en alcanzar autonomía política, alimentaria y económica
+                mediante el reciclaje y la producción local.
+              </p>
+              <button>Ver ubicaciones</button>
+            </div>
+            <div className={s.card}>
+              <Image src={bolson} alt="ccr" />
+              <h3>Reciclado PET</h3>
+              <p>
+                Este proyecto busca convertir Centros Comunitarios en
+                resilientes, logrando un impacto social y ambiental positivo.
+              </p>
+              <button>Saber más</button>
+            </div>
+            <div className={s.card}>
+              <Image src={barrioLimpio} alt="ccr" />
+              <h3>Barrio Limpio</h3>
+              <p>
+                Diseñar de manera participativa una metodología para recolectar,
+                acopiar y clasificar la basura, realizando una prueba piloto en
+                el Barrio Alte Brown, Tigre Sur que sirva como modelo para el
+                diseño del Programa Barrio Limpio.
+              </p>
+              <button>Saber más</button>
+            </div>
+            <div className={s.card}>
+              <Image src={barrioLimpio} alt="ccr" />
+              <h3>Otro proyecto</h3>
+              <p>Poner una cuarta card. No se de que puede ser pero agregar</p>
+              <button>Saber más</button>
+            </div>
+          </div>
         </div>
       </section>
     </>
