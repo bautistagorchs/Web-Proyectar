3cea0c579b087ea9984eaf7413de722246ad230f
bautistagorchs <bautista.gorchs@gmail.com>
2024-08-02 14:10:14 -0300

    exra

--- src/componentes/landing/Landing.tsx
+++ src/componentes/landing/Landing.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import s from "./landing.module.scss";
-import Image from "next/image";
-import img from "@/archivos/niño_parado_espaldas.jpg";
+// import Image from "next/image";
+// import img from "@/archivos/niño_parado_espaldas.jpg";
 
 export default function Landing() {
   return (
     <main className={s.main}>
-      <Image src={img} alt="hsd" className={s.video} />
-      {/* <video autoPlay loop muted className={s.video}>
+      {/* <Image src={img} alt="hsd" className={s.video} /> */}
+      <video autoPlay loop muted className={s.video}>
         <source
           src={
             "https://videos.pexels.com/video-files/6753733/6753733-uhd_2560_1440_30fps.mp4"
@@ -17,7 +17,7 @@ export default function Landing() {
           type="video/mp4"
         />
         Your browser does not support the video tag.
-      </video> */}
+      </video>
       <div className={s.mainContent}>
         <div className={s.left}>
           <h1>¿Que planeta le dejamos a nuestros hijos?</h1>
