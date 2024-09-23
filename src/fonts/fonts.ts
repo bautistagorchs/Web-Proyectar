caf3b42a2864e900de58e8aab8ec52564d8dc2bc
bautistagorchs <bautista.gorchs@gmail.com>
2024-09-22 23:40:43 -0300

    limpieza de proyecto

---
+++ src/fonts/fonts.ts
@@ -0,0 +1,26 @@
+import { Open_Sans } from "next/font/google";
+
+// export const source_code_pro = Source_Code_Pro({
+//   subsets: ["latin"],
+//   display: "swap",
+// });
+
+export const open_sans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+// export const poppins = Poppins({
+//   subsets: ["latin"],
+//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+// });
+
+// export const roboto = Roboto_Slab({
+//   subsets: ["latin"],
+//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+// });
+
+// export const code = Fira_Code({
+//   subsets: ["latin"],
+//   weight: ["300", "400", "500", "600", "700"],
+// });
